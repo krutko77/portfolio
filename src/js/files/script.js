633f482380840c8abbbdaa780b017e27a731de42
krutko77 <70036691+krutko77@users.noreply.github.com>
2022-05-09 19:44:46 +0300

    add transition portfolio__item--lower

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,8 +1,3 @@
-// // Подключение функционала "Чертогов Фрилансера"
-// import { isMobile } from "./functions.js";
-// // Подключение списка активных модулей
-// import { flsModules } from "./modules.js";
-
 //show more
 
 const gallery = document.querySelectorAll(".portfolio__hide");
@@ -24,33 +19,16 @@ function galleryHide() {
 }
 
 function showMoreBtn() {
-
    showMoreText.classList.toggle('btn--show');
     if(showMoreText.classList.contains('btn--show')) {
       showMoreText.innerHTML = 'Скрыть';
-      iconMore.classList.remove('fa-chevron-down');
-      iconMore.classList.add('fa-chevron-up');
+      iconMore.classList.remove('_icon-chevron-down');
+      iconMore.classList.add('_icon-chevron-up');
     } 
     else {
       showMoreText.innerHTML = 'Показать еще';
-      iconMore.classList.remove('fa-chevron-up');
-      iconMore.classList.add('fa-chevron-down');
-    } 
-    
+      iconMore.classList.remove('_icon-chevron-up');
+      iconMore.classList.add('_icon-chevron-down');
+    }     
   }
 
-
-// function galleryHide() {
-//    if(portfolio__hide.classList.contains('portfolio__hide')) {
-//       portfolio__hide.classList.remove('portfolio__hide');
-//       portfolio__button-more.classList.add('button__close');
-//    }
-//    else {
-//       portfolio__hide.classList.add('portfolio__hide');
-//       portfolio__button-more.classList.remove('button__close');
-//       portfolio__button-more.classList.add('button__show');
-//    }
-   
-// }
-
-// galleryHide();
