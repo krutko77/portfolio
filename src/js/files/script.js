c1023ee88061cd81d1a76358f8c7db6464b073f7
krutko77 <70036691+krutko77@users.noreply.github.com>
2022-05-14 16:16:53 +0300

    section about

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,1 +1,14 @@
-//
+//skills transition
+const skillsContent = document.querySelector('.skills__content');
+skillsContent.classList.remove('square-transition');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      skillsContent.classList.add('_content-transition');
+      return;
+    }
+    skillsContent.classList.remove('_content-transition');
+  });
+});
+observer.observe(document.querySelector('.skills__content'));
