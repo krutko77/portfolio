c0efc9f89ced8ffba77c38d210fe34ebe51ae85d
krutko77 <70036691+krutko77@users.noreply.github.com>
2022-05-28 22:49:13 +0300

    js corrected

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,7 +1,58 @@
+
 // очистка полей формы
-document.addEventListener('submit', (e) => { 
-   // Отключаем событие по умолчанию 
-       e.prevent.Default(); 
-   // Очищаем поля формы 
-       e.target.reset(); 
-   });
+
+// const form = document.getElementById("form");
+
+// form.addEventListener('submit', (e) => { 
+//    // Отключаем событие по умолчанию 
+//        e.prevent.Default(); 
+//    // Очищаем поля формы 
+//        e.target.reset(); 
+//    });
+
+
+
+let button = document.querySelector('.clear-button'); 
+
+button.addEventListener("click", submitForm);
+
+	function submitForm() {
+      document.getElementById('name').value='';
+      document.getElementById('email').value='';
+      document.getElementById('message').value='';
+   }
+		
+	
+// function fun() {
+//    document.getElementById('name').value='';
+//    document.getElementById('mail').value='';
+//    document.getElementById('message').value='';
+//    document.getElementById("form").reset();
+//    }
+
+
+
+
+// function testSubmit() { 
+//    var x = document.forms["myForm"]["input1"]; 
+//    var y = document.forms["myForm"]["input2"]; 
+   
+//    if (x.value === "") { 
+//       alert('plz fill!!'); 
+//       return false; 
+//       } 
+
+//    if(y.value === "") { 
+//       alert('plz fill the!!'); 
+//       return false; 
+//    } 
+   
+//    return true; 
+// } 
+         
+// function submitForm() {
+//       if (testSubmit()) { 
+//       document.getElementById("form").submit();//сначала отправьте 
+//       document.getElementById("form").reset(); // а затем сбросьте значения формы 
+//    } 
+// } 
